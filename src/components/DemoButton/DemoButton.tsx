788de41997c9fec17206f64b9fe593044062db8d
Carlton Lindsay <46255824+Carlton-L@users.noreply.github.com>
2025-05-28 17:27:06 +0200

    Form submission

--- src/components/DemoButton/DemoButton.tsx
+++ src/components/DemoButton/DemoButton.tsx
@@ -1,4 +1,5 @@
 import {
+  Badge,
   CloseButton,
   Dialog,
   Field,
@@ -27,47 +28,91 @@ const DemoButton = () => {
               </Dialog.Title>
             </Dialog.Header>
             <Dialog.Body>
-              <Fieldset.Root size='lg' maxW='md'>
-                <Stack>
-                  <Fieldset.Legend color='white'>
-                    Contact details
-                  </Fieldset.Legend>
-                  <Fieldset.HelperText color='#646E78'>
-                    Please provide your contact details below.
-                  </Fieldset.HelperText>
-                </Stack>
+              <form
+                action='https://formsubmit.co/m@futurity.systems'
+                method='POST'
+              >
+                <Fieldset.Root size='lg' maxW='md'>
+                  <Stack>
+                    <Fieldset.Legend color='white'>
+                      Contact details
+                    </Fieldset.Legend>
+                    <Fieldset.HelperText color='#646E78'>
+                      Please provide your contact details below.
+                    </Fieldset.HelperText>
+                  </Stack>
 
-                <Fieldset.Content color='white'>
-                  <Field.Root>
-                    <Field.Label>Full Name</Field.Label>
-                    <Input name='name' />
-                  </Field.Root>
+                  <Fieldset.Content color='white'>
+                    <Field.Root required>
+                      <Field.Label>
+                        Full Name
+                        <Field.RequiredIndicator />
+                      </Field.Label>
+                      <Input name='name' />
+                    </Field.Root>
 
-                  <Field.Root>
-                    <Field.Label>Email address</Field.Label>
-                    <Input name='email' type='email' />
-                  </Field.Root>
+                    <Field.Root required>
+                      <Field.Label>
+                        Email address
+                        <Field.RequiredIndicator />
+                      </Field.Label>
+                      <Input name='email' type='email' />
+                    </Field.Root>
 
-                  <Field.Root>
-                    <Field.Label>Role</Field.Label>
-                    <Input name='role' />
-                  </Field.Root>
+                    <Field.Root>
+                      <Field.Label>
+                        Role
+                        <Field.RequiredIndicator
+                          fallback={
+                            <Badge
+                              size='xs'
+                              variant='outline'
+                              background='#1a1a1a'
+                              color='#A7ACB2'
+                              boxShadow='inset 0 0 0px 1px #333333'
+                            >
+                              Optional
+                            </Badge>
+                          }
+                        />
+                      </Field.Label>
+                      <Input name='role' />
+                    </Field.Root>
 
-                  <Field.Root>
-                    <Field.Label>Company</Field.Label>
-                    <Input name='company' />
-                  </Field.Root>
+                    <Field.Root required>
+                      <Field.Label>
+                        Company
+                        <Field.RequiredIndicator />
+                      </Field.Label>
+                      <Input name='company' />
+                    </Field.Root>
 
-                  <Field.Root>
-                    <Field.Label>Message (Optional)</Field.Label>
-                    <Textarea placeholder='Type your message here' />
-                  </Field.Root>
-                </Fieldset.Content>
+                    <Field.Root>
+                      <Field.Label>
+                        Message
+                        <Field.RequiredIndicator
+                          fallback={
+                            <Badge
+                              size='xs'
+                              variant='outline'
+                              background='#1a1a1a'
+                              color='#A7ACB2'
+                              boxShadow='inset 0 0 0px 1px #333333'
+                            >
+                              Optional
+                            </Badge>
+                          }
+                        />
+                      </Field.Label>
+                      <Textarea placeholder='Type your message here' />
+                    </Field.Root>
+                  </Fieldset.Content>
 
-                <Button type='submit' alignSelf='flex-start'>
-                  Submit
-                </Button>
-              </Fieldset.Root>
+                  <Button type='submit' alignSelf='flex-start'>
+                    Submit
+                  </Button>
+                </Fieldset.Root>
+              </form>
             </Dialog.Body>
             <Dialog.CloseTrigger asChild>
               <CloseButton size='sm' />
