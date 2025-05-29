9308ba5747685def65eb253ffe67fa3e0502ddbf
Carlton Lindsay <46255824+Carlton-L@users.noreply.github.com>
2025-05-29 10:47:41 +0200

    Removed font fallback reference in colors.tsx

--- src/theme/tokens/colors.tsx
+++ src/theme/tokens/colors.tsx
@@ -1,7 +1,6 @@
 import { defineTokens } from '@chakra-ui/react';
 
 // TODO: Update fallback fonts
-const fallback = `-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`;
 
 const colors = defineTokens.colors({});
 
